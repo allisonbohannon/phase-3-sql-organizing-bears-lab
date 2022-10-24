bacf13a715005cb5300fe390ce32978c4b572d2f
Allison Bohannon <akboh.github@gmail.com>
2022-10-23 21:16:57 -0700

    commit tree 7fab5cf84425f5d1ad104345f5123758671b8ec6

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 10, "M", "brown", "mean", 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 9, "M", "black", "mean", 1  ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 8, "F", "brown", "mean", 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 7, "F", "black", "mean", 1  ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 6, "F", "white", "mean", 1  ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 5, "M", "brown", "mean", 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 4, "F", "black", "mean", 1 ); 
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 3, "M", "white", "mean", 1 );
